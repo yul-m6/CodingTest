c02031fdd98df825d5a283b61588133f7955452d
6udguin <ywsohms7745@gmail.com>
2024-08-03 12:05:38 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.05 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public List solution(int[] num_list, int n) {
+        
+        List<Integer> answer = new ArrayList<Integer>();
+        
+        for(int i=0; i<num_list.length; i++){
+            if(i < n){
+                answer.add(num_list[i]);
+            }
+        }
+        return answer;
+    }
+}
