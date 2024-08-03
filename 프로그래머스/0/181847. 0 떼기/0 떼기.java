d43abc6fe6ac6aeca5017b4869d7153079e31444
6udguin <ywsohms7745@gmail.com>
2024-08-03 18:56:17 +0900

    [level 0] Title: 0 떼기, Time: 0.05 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = String.valueOf(Integer.parseInt(n_str));
+        
+        
+        
+        return answer;
+    }
+}
