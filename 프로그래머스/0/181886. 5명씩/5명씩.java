3da1bd5298bffeeef9acb82900e91d22627b940b
6udguin <ywsohms7745@gmail.com>
2024-08-03 12:01:15 +0900

    [level 0] Title: 5명씩, Time: 0.05 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -1,21 +1,13 @@
 import java.util.*;
 class Solution {
-    public String[] solution(String[] names) {
-        String[] answer = {};
-        int arrCnt = 0;
-        List<String> targetName = new ArrayList<String>();
+    public List solution(String[] names) {
         
-        for(int i=0; i<names.length; i++){
-            if(i%5==0){
-                arrCnt++;
-                targetName.add(names[i]);
-            }    
-        }
-        answer = new String[arrCnt];
+        List<String> answer = new ArrayList<String>();
         
-        for(int i=0; i<targetName.size(); i++){
-            answer[i] = targetName.get(i);
+        for(int i=0; i<names.length; i+=5){
+            answer.add(names[i]);
         }
+        
         
         return answer;
     }
